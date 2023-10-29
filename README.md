0bd81f41467b4dc3250c2d460310b7e180e964bf
leonardofj <leonardofj@gmail.com>
2023-10-29 23:31:41 +0100

    chore: adding prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # best-nine-app
+
 Creating a collage of best 9 posts from Instagram
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const Koa = require("koa");
-const nunjucks = require('koa-nunjucks-2');
+const nunjucks = require("koa-nunjucks-2");
 const router = require("./router");
 const App = new Koa();
 const port = 8000;
 
-App.use(nunjucks({
-    ext: 'njk',
-    path: __dirname + '/views',
-}));
+App.use(
+  nunjucks({
+    ext: "njk",
+    path: __dirname + "/views",
+  }),
+);
 
 App.use(router.routes());
 App.listen(port, () => {
-    console.log(`🚀 Server listening http://127.0.0.1:${port}/ 🚀`);
+  console.log(`🚀 Server listening http://127.0.0.1:${port}/ 🚀`);
 });
