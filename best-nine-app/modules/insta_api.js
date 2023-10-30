8acf3e87d7bc5eb894e742f7acce2b81bf8c076d
leonardofj <leonardofj@gmail.com>
2023-10-30 13:18:16 +0100

    chore: reorganizing project structure

--- best-nine-app/modules/insta_api.js
+++ best-nine-app/modules/insta_api.js
@@ -61,7 +61,7 @@ const get_user_id = async (username) => {
         "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)",
     };
     const response = await axios.get(url, { headers });
-    return response.data.data.user.id;
+    return response.data.data?.user?.id;
   } catch (error) {
     console.log(error);
     return "";
@@ -92,7 +92,7 @@ async function* get_user_posts(user_id, year = 0) {
     }
 
     const responseData = response.data;
-    posts = responseData.data.user.edge_owner_to_timeline_media.edges;
+    posts = responseData.data?.user?.edge_owner_to_timeline_media?.edges;
 
     for (const post of posts) {
       if (year) {
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Koa = require("koa");
 const nunjucks = require("koa-nunjucks-2");
-const router = require("./router");
+const router = require("./best-nine-app/router");
 const App = new Koa();
 const port = 8000;
 
 App.use(
   nunjucks({
     ext: "njk",
-    path: __dirname + "/views",
+    path: __dirname + "/best-nine-app/views",
   }),
 );
 
